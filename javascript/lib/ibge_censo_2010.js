e431bba77521391e6d54b9b8752278b0ed4be3f3
Alexandre Gomes <alegomes@gmail.com>
2011-05-08 11:26:22 -0300

    literates specs

--- javascript/lib/ibge_censo_2010.js
+++ javascript/lib/ibge_censo_2010.js
@@ -1,49 +1,78 @@
-var IBGECenso2010 = function() {
-	
-}
-
-IBGECenso2010.prototype = {
+var IBGECenso2010 =  {
 	
 	sex_ratio: function() {
 		var URL = "http://www.censo2010.ibge.gov.br/sinopse/index.php?dados=1R&uf=00";
-
+		htmldoc = this.open_html_doc(URL);
+		return IBGECenso2010SexRatio.get_instance(htmldoc);
+	},
+	
+	literacy: function() {
+		var URL = "http://www.censo2010.ibge.gov.br/sinopse/index.php?dados=P6&uf=00";
+		htmldoc = this.open_html_doc(URL);
+		return IBGECenso2010LiteratesRatio.get_instance(htmldoc);
+	},
+	
+	open_html_doc: function(url) {
 		var xhr = new XMLHttpRequest();
-		xhr.open("GET", URL, false);
+		xhr.open("GET", url, false);
 		xhr.send();
 		
-		var htmldoc = xhr.responseText;
-		
-		var parser = new DOMParser();
-		var xmldoc = parser.parseFromString(htmldoc, "text/xml");
-
-		return new IBGECenso2010SexRatio(htmldoc);
+		return xhr.responseText;
 	}
 }
 
-var IBGECenso2010SexRatio = function(htmldoc) {
-	this.content = htmldoc;
+
+var IBGECenso2010SexRatio = {
+
+	get_instance: function(htmldoc) {
+		this.content = htmldoc;
+		return this;
+	},
+	
+	women_per_region: function() {
+		return get_census_values(this,5,8,11,14,17);
+	},
+	
+	men_per_region: function() {
+		return get_census_values(this,4,7,10,13,16);
+	}
 }
 
-IBGECenso2010SexRatio.prototype = {
-	women_per_region: function() {
-		
-		var td_numbers_global = /<td class="td_numeros".*>.*<\/td>/g
-		var td_numbers_local = /<td class="td_numeros".*>(.*)<\/td>/
-		
-		numbers = this.content.match(td_numbers_global);
-		
-		var women_in_n = numbers[5].match(td_numbers_local)[1].replace(/\./g, '');
-		var women_in_ne = numbers[8].match(td_numbers_local)[1].replace(/\./g, '');
-		var women_in_s = numbers[11].match(td_numbers_local)[1].replace(/\./g, '');
-		var women_in_se = numbers[14].match(td_numbers_local)[1].replace(/\./g, '');
-		var women_in_co = numbers[17].match(td_numbers_local)[1].replace(/\./g, '');
-		
-		return {
-			"N"  : women_in_n,
-			"NE" : women_in_ne,
-			"S"	 : women_in_s,
-			"SE" : women_in_se,
-			"CO" : women_in_co
-		}
+
+var IBGECenso2010LiteratesRatio = {
+	
+	get_instance: function(htmldoc) {
+		this.content = htmldoc;
+		return this;
+	},
+	
+	literates_per_region: function() {
+		return get_census_values(this,9,15,21,27,33);
+	},
+	
+	not_children_per_region: function() {
+		return get_census_values(this,6,12,18,24,30);
+	}
+	
+}
+
+get_census_values = function(page,n,ne,se,s,co) {
+	var td_numbers_global = /<td class="td_numeros".*>.*<\/td>/g
+	var td_numbers_local = /<td class="td_numeros".*>(.*)<\/td>/
+
+	numbers = page.content.match(td_numbers_global);
+
+	var in_n = numbers[n].match(td_numbers_local)[1].replace(/\./g, '');
+	var in_ne = numbers[ne].match(td_numbers_local)[1].replace(/\./g, '');
+	var in_se = numbers[se].match(td_numbers_local)[1].replace(/\./g, '');
+	var in_s = numbers[s].match(td_numbers_local)[1].replace(/\./g, '');
+	var in_co = numbers[co].match(td_numbers_local)[1].replace(/\./g, '');
+
+	return {
+		"N"  : in_n,
+		"NE" : in_ne,
+		"SE" : in_se,
+		"S"	 : in_s,
+		"CO" : in_co
 	}
 }
--- javascript/spec/unit/ibge_censo_2010_spec.js
+++ javascript/spec/unit/ibge_censo_2010_spec.js
@@ -3,25 +3,69 @@ describe 'IBGECenso2010'
   describe '.sex_ratio'
 
 	before_each
-		censo = new IBGECenso2010();
-	  	sex_ratio = censo.sex_ratio();
+		// censo = new IBGECenso2010();
+	  	sex_ratio = IBGECenso2010.sex_ratio();
 	end
 
     it 'should open with "Razão de sexo, população de homens e mulheres"'
 		sex_ratio.should.not.be_null
 		sex_ratio.content.should.match /Razão de sexo, população de homens e mulheres/
     end
-
+	
 	it 'should get number of women' 
 		var women = sex_ratio.women_per_region()
 	
 		women["N"].should.eql "7859539"
 		women["NE"].should.eql "27172904"
-		women["S"].should.eql "41287763"
-		women["SE"].should.eql "13950480"
+		women["SE"].should.eql "41287763"
+		women["S"].should.eql "13950480"
 		women["CO"].should.eql "7078123"
+	end
+	
+	it 'should get number of men' 
+		var men = sex_ratio.men_per_region()
+	
+		men["N"].should.eql "8004915"
+		men["NE"].should.eql "25909046"
+		men["SE"].should.eql "39076647"
+		men["S"].should.eql "13436411"
+		men["CO"].should.eql "6979971"
 		
 	end
 
   end
+
+  describe '.literates_ratio'
+  	
+    	before_each
+    	  	literacy = IBGECenso2010.literacy()
+    	end
+  		
+  		it 'should open page with "Pessoas de 10 anos ou mais de idade, total e alfabetizadas, por situação do domicílio, segundo as Grandes Regiões e as Unidades da Federação - 2010"'
+  			literacy.should.not.be_null
+  			literacy.content.should.match /Pessoas de 10 anos ou mais de idade, total e alfabetizadas, por situação do domicílio, segundo as Grandes Regiões e as Unidades da Federação - 2010/
+  		end
+  
+  		it 'should get number of readers > 10 yo'
+  			literates = literacy.literates_per_region()
+  			
+  			literates["N"].should.eql "11326492"
+  			literates["NE"].should.eql "36418124"
+  			literates["SE"].should.eql "65979578"
+  			literates["S"].should.eql "22571641"
+  			literates["CO"].should.eql "11082347"
+  		end
+		
+		it 'should get population > 10 yo'
+			
+			population = literacy.not_children_per_region()
+			
+			population["N"].should.eql "12670041"
+			population["NE"].should.eql "44223349"
+			population["SE"].should.eql "69532602"
+			population["S"].should.eql "23694211"
+			population["CO"].should.eql "11870062"
+		end
+		
+	end
 end
